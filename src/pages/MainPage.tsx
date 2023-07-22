3156c3d6e50fe8ee2a5751b813192df867df18c4
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-07-22 19:48:25 +0300

    feat(pages): add component Artiles

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Medicines from 'components/medicines/medicines';
 
 export default function MainPage() {
@@ -11,5 +12,38 @@ export default function MainPage() {
       {/* Лекарства и БАД */}
       {/* Спроси врача */}
     </>
+=======
+import Articles from 'components/articles/articles';
+
+export default function MainPage() {
+  return (
+    //
+    // Самое популярное и новости
+    //
+
+    //
+    // Карусель
+    //
+
+    //
+    <Articles />
+    //
+
+    //
+    // Подкасты
+    //
+
+    //
+    // Подписка
+    //
+
+    //
+    // Лекарства и БАД
+    //
+
+    //
+    // Задать вопрос врачу
+    //
+>>>>>>> 2dfd45e (feat(pages): add component Artiles)
   );
 }
