108d2e0b91e068ef4cab576676ad3a56ec9ca758
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-07-22 22:44:06 +0300

    refactor(pages/mainpage): change structure with comments

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,16 @@
 import Articles from 'components/articles/articles';
-import News from 'components/news/news';
+import AskDoctor from 'components/ask-doctor/ask-doctor';
 
 export default function MainPage() {
   return (
     <>
-      <News />
+      {/* Новости */}
       {/* Карусель */}
       <Articles />
       {/* Подкасты */}
       {/* Подписка */}
       {/* Лекарства и БАД */}
-      {/* Спроси врача */}
+      <AskDoctor />
     </>
   )
 }
